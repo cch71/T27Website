c79a8fe0c3c1abaf461a11ad8ede90b9ca5df7e2
Craig Hamilton <cch71a@gmail.com>
2023-10-12 10:53:18 -0500

    Updating default styles

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,36 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  elements: {
+    text: {
+      secondary: {
+        color: {
+          static: {
+            initial: '#000',
+            dark: '#ADA9A4'
+          }
+        }
+      }
+    }
+  },
+
+  prose: {
+    li: {
+      margin: '5px '
+    },
+
+    table: {
+      margin: '14px 0',
+      lineHeight: '.6rem'
+    }
+  },
+
+  docus: {
+    header: {
+      title: {
+        fontSize: '1.2rem',
+        fontWeight: '500'
+      }
+    }
+  }
 })
