38ef00bbc19326d8e04906a793e35b781cb76d7c
Craig Hamilton <cch71a@gmail.com>
2023-10-29 20:31:53 -0600

    setting the whole site background color

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,36 +1,44 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
-  elements: {
-    text: {
-      secondary: {
-        color: {
-          static: {
-            initial: '{color.gray.900}',
-            dark: '#ADA9A4'
-          }
-        }
-      }
-    }
-  },
+	elements: {
+		text: {
+			secondary: {
+				color: {
+					static: {
+						initial: '{color.gray.900}',
+						dark: '#ADA9A4'
+					}
+				}
+			}
+		},
+		backdrop: {
+			background: '#f4f5f4'
+		}
+	},
 
-  prose: {
-    li: {
-      margin: '5px '
-    },
+	prose: {
+		li: {
+			margin: '5px '
+		},
 
-    table: {
-      margin: '14px 0',
-      lineHeight: '.6rem'
-    }
-  },
+		table: {
+			margin: '14px 0',
+			lineHeight: '.6rem'
+		}
+	},
 
-  docus: {
-    header: {
-      title: {
-        fontSize: '1.2rem',
-        fontWeight: '500'
-      }
-    }
-  }
+	docus: {
+		body: {
+			backgroundColor: {
+				initial: '#f4f5f4'
+			},
+		},
+		header: {
+			title: {
+				fontSize: '1.2rem',
+				fontWeight: '500'
+			}
+		}
+	}
 })
