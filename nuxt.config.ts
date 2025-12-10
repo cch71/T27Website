264c52b4b8e008fbb41e43bc4aebf9aa8f6a2517
Craig Hamilton <chamilton@craigscreations.com>
2025-12-09 19:40:36 -0700

    Adding new eagle scout and wintercamp signup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,19 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
-	extends: '@nuxt-themes/docus',
+  extends: "@nuxt-themes/docus",
 
   devtools: { enabled: true },
 
-  css: [
-									'@/assets/css/main.css',
-	],
+  css: ["@/assets/css/main.css"],
 
   colorMode: {
-									preference: "light"
-	},
+    preference: "light",
+  },
 
   site: {
-									url: 'https://www.bsatroop27.us/'	
-	},
+    url: "https://www.bsatroop27.us/",
+  },
 
-  modules: [ '@nuxtjs/robots', '@nuxtjs/sitemap'],
-  compatibilityDate: '2024-09-10'
-})
+  modules: ["@nuxtjs/robots", "@nuxtjs/sitemap"],
+  compatibilityDate: "2024-09-10",
+});
